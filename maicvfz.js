4200db4ec880b0bcf4a9f1868e8de8fe31521d6a
franklinlee88888888-a11y <franklinlee88888888@gmail.com>
2025-09-16 06:17:15 +0200

    Add files via upload

---
+++ maicvfz.js
@@ -0,0 +1,178 @@
+// @FXML private  TextArea t5,t1;
+	
+	
+// 	private static final Map<Character, String> MO= new HashMap<>();
+	
+// 	static {
+// 		MO.put('W', "UP");
+// 		MO.put('A',"LEFT");
+// 		MO.put('S', "DOWN");
+// 		MO.put('D',"RIGHT");
+// 		MO.put('I', "UP"    );   //*7
+// 		MO.put('J',"LEFT"  );
+// 		MO.put('K', "DOWN");
+// 		MO.put('L',"RIGHT");
+// 		MO.put('8', "UP");
+// 		MO.put('4',"LEFT");
+// 		MO.put('6', "RIGHT");
+// 		MO.put('2',"DOWN");
+// 		    /*-+7**/
+// 	}  //*7 ? “œ	>? //*a7}S/faystem.out.print(Map);
+	
+// 	public static   String  texctomn(String text){
+		
+// 		try {StringBuilder out = new StringBuilder();
+// 		String upper  =  text.toUpperCase();
+		
+// 	     boolean ftoken  =   true;
+// 	           for(int i = 0; i<text.length();  i++) {
+// 	        	      char ch =   upper.charAt(i); /*7**/System.out.print(ch);
+	        	      
+// 	        	      if(   ch  == ' ') {
+// 	        	    	     out.append("   ");
+// 	        	    	       ftoken= true;
+// 	        	    	            continue;
+// 	        	      }
+// 	        	          String codf=  MO.get(ch);
+// 	        	            if( codf!=  null)  {
+	        	            	
+	        	            	
+	        	            	
+// 	        	            	   if(!ftoken  ) out.append(' ');/*+741-*7**/
+// 	        	            	   out.append(codf);
+// 	        	            	      ftoken =  false;
+// 	        	            }
+// 	        	            }
+// 	           return out.toString();//}
+// 		}catch(Exception e) {
+			
+			
+// 			System.out.print(e);return "Error";
+// 		}
+
+// Map<CharacterData, String>=? MO;//= new HashMap<>();
+
+// MO.put('W', "UP");
+//  		MO.put('A',"LEFT");
+//  		MO.put('S', "DOWN");
+//  		MO.put('D',"RIGHT");
+//  		MO.put('I', "UP"    );   //*7
+//  		MO.put('J',"LEFT"  );
+//  		MO.put('K', "DOWN");
+// /*---*/		MO.put('L',"RIGHT");
+//  		MO.put('8', "UP");
+// 		   MO.put('4',"LEFT");
+//  		MO.put('6', "RIGHT");
+//  		MO.put('2',"DOWN");
+//         console.log(MO)
+
+
+
+
+  const MO= {'W': "UP"  /*-+7**/,
+  'A':   "LEFT",
+    'S':   "DOWN"    ,/*7--p *7kl**/
+ /*--*/'D':"RIGHT"
+
+}
+console.log(       MO)  ;
+
+//*7 ,vb?c      S/tring text   = t1.gettex
+
+// for(int i=0; i<text.length();  i++) {
+//     	        	      char ch =   upper.charAt(i); /*7**/System.out.print(ch);
+                          
+//     	        	      if(   ch  == ' ') {
+//     	        	    	     out.append("   ");
+//     	        	    	       ftoken= true;
+//     	        	    	            continue;
+//     	        	      }
+//     	        	          String codf=  MO.get(ch);
+//     	        	            if( codf!=  null)  {
+                                    
+                                    
+                                    
+//     	        	            	   if(!ftoken  ) out.append(' ');/*+741-*7**/
+//     	        	            	   out.append(codf);
+//     	        	            	      ftoken =  false;
+//     	        	            }
+//     	        	            }
+//     	           return out.toString();
+const injf = document.getElementById("t1").value
+function texctomn(){
+
+    //*7value.toUppercase() ;//*.nodeValue.getElementById("t1")
+    //*-7console.log(  injf)
+
+    //boolean fttoken;
+	          /*-7**/// for(int i; i<injf.length()  i++) {
+	        	      
+	        	      
+	        	      let ch  = "";
+                  for(let char of injf){
+                    if(MO[char])  {
+
+                        ch+=  MO[char] /*-- +7 ***/+"  ";
+                    }
+                  }    document.getElementById("t5"  ).value=    ch.trim(	);
+				  console.log(  document.getElementById("t5"  ).value)
+	        	    	//      out.append("   ");
+	        	    	//        ftoken= true;
+	        	    	//             continue;
+	        	      // }
+	        	      //     string codf=  MO.get(ch);
+	        	      //       if( codf!=  null)  {
+	        	            	
+	        	            	
+	        	            	
+	        	      //       	   if(!ftoken  ) out.append(' ');/*+741-*7**/
+	        	      //       	   out.append(codf);
+	        	      //       	      //ftoken =  false;
+	        	      //       }
+	        	            }
+	           //*7Xreturn out.toString();//}
+		//}
+    
+//*7 >/v ccS}
+
+function ttot() {
+	
+}
+     const move1= document.getElementById("move1")
+const Mva= 20;
+
+let      x=  0;
+
+let y   =0   ;
+
+document.addEventListener   ("keydown",  event   =>
+{
+
+	if (event.key.startsWith("Arrow")) {
+		switch(event.key){
+			case  "Arrowup"  :
+
+
+			y-=Mva
+			break;
+
+
+			case  "Arrowdown"  :
+			y+=Mva
+			break;
+
+
+
+			  case  "Arrowup"        :
+			x-=Mva
+			break;
+
+
+			case  "Arrowdown"  :
+			x  +=Mva
+			break;
+		}
+
+		move1.style.top=  `${y}px`;/*---*//*7 ?‘/f*a7 7894 **/   move1.style.left=  `${x}px`
+	}
+});  //*-fa7 ?‘“”§m1095654  mnzdzSzdCSEWWQAWSasfigft beklow ads acz abovc lorevcmm, 
